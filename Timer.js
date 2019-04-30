2ec68b95138be0c0384b118a99838fb6bc0db51e
MrKamake <seongjooon@gmail.com>
2019-04-30 10:50:29 +0900

    haha

--- Timer.js
+++ Timer.js
@@ -1,3 +1,5 @@
-const oneSeconds = setInterval(()=>{console.log('b')}, 1000);
-console.log(oneSeconds)
+const userTime = 2;
 
+
+const oneSeconds = setTimeout(() => { console.log('Brrrrrrrr') }, userTime * 1000);
+console.log(oneSeconds)
